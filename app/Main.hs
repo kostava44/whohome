7ddde942d25751f7652dd37ad3535751bb5e80eb
wldhx <wldhx@wldhx.me>
2022-12-08 20:59:38 +0000

    connect

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
 import Control.Monad (forever)
 import Control.Monad.Loops (whileJust_)
 import Data.ByteString.Char8 qualified as B8
-import Network.Simple.TCP qualified as TCP (Socket, connect, recv, send)
+import Network.Simple.TCP qualified as TCP (HostName, Socket, connect, recv, send)
 import Network.Telnet.LibTelnet qualified as Telnet
 
 -- handle <- connect host
@@ -29,17 +29,21 @@ handle state@(State _ done) s = do
     Telnet.telnetRecv telnet bs
   putMVar done ()
 
+connect :: TCP.HostName -> (State -> IO a) -> IO ()
+connect host f = do
+  x <- newEmptyMVar
+  done <- newEmptyMVar
+  let state = State x done
+  concurrently_
+    (TCP.connect host "telnet" (\(s, _) -> handle state s))
+    ( race_
+        (takeMVar done)
+        (f state)
+    )
+
 recv :: State -> IO B8.ByteString
 recv (State readBuf _) = takeMVar readBuf
 
 main :: IO ()
 main = do
-  x <- newEmptyMVar
-  done <- newEmptyMVar
-  let state = State x done
-  concurrently_
-    (TCP.connect "127.0.0.1" "telnet" (\(s, _) -> handle state s))
-    ( race_
-        (takeMVar done)
-        (forever $ recv state >>= B8.putStr)
-    )
+  connect "127.0.0.1" (\s -> forever $ recv s >>= B8.putStr)
