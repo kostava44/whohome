5f43aeb8d3c60537f9fdf6f2e82536f3fe011920
wldhx <wldhx@wldhx.me>
2022-12-08 20:46:35 +0000

    add state

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,40 @@
-{-# LANGUAGE OverloadedStrings#-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
+import Control.Concurrent.Async ( concurrently_, race_ )
+import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
+import Control.Monad (forever)
 import Control.Monad.Loops (whileJust_)
-import qualified Data.ByteString.Char8 as B8
-import           Network.Simple.TCP (Socket, recv, send, connect)
-import qualified Network.Telnet.LibTelnet as Telnet
+import Data.ByteString.Char8 qualified as B8
+import Network.Simple.TCP (Socket, connect, recv, send)
+import Network.Telnet.LibTelnet qualified as Telnet
 
+-- handle <- connect host
+-- send handle bs
+-- bs <- recv handle
 
-telnetH :: Socket -> Telnet.EventHandler
-telnetH _ _ (Telnet.Received b)
-  = putStr "R: " *> B8.putStrLn b
-telnetH s _ (Telnet.Send b)
-  = putStr "S: " *> send s b
-telnetH _ _ _ = pure ()
+telnetH :: MVar B8.ByteString -> Socket -> Telnet.EventHandler
+telnetH state _ _ (Telnet.Received b) =
+  putStr "R: " *> B8.putStrLn b *> putMVar state b
+telnetH _ s _ (Telnet.Send b) =
+  putStr "S: " *> send s b
+telnetH _ _ _ _ = pure ()
+
+handle :: MVar B8.ByteString -> MVar () -> Socket -> IO ()
+handle state done s = do
+  telnet <- Telnet.telnetInit [] [] (telnetH state s)
+  whileJust_ (recv s 4096) $ \bs -> do
+    Telnet.telnetRecv telnet bs
+  putMVar done ()
 
 main :: IO ()
-main = connect "127.0.0.1" "telnet" (\(s, _) -> handle s) where
-  handle s = do
-      telnet <- Telnet.telnetInit [] [] (telnetH s)
-      whileJust_ (recv s 4096) $ \bs -> do
-          Telnet.telnetRecv telnet bs
+main = do
+  state <- newEmptyMVar
+  done <- newEmptyMVar
+  concurrently_
+    (connect "127.0.0.1" "telnet" (\(s, _) -> handle state done s))
+    ( race_
+        (takeMVar done)
+        (forever $ takeMVar state >>= B8.putStrLn)
+    )
