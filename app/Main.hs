b1d9a9e4f84e89a6643a29591f81dad94c48f942
wldhx <wldhx@wldhx.me>
2022-12-08 20:14:56 +0000

    telnet reader

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,15 @@
 
 module Main where
 
+import Control.Monad.Loops (whileJust_)
 import qualified Data.ByteString.Char8 as B8
-import           Network.Simple.TCP (Socket, send, connect)
+import           Network.Simple.TCP (Socket, recv, send, connect)
 import qualified Network.Telnet.LibTelnet as Telnet
 
 
 telnetH :: Socket -> Telnet.EventHandler
-telnetH _ t (Telnet.Received b)
-  = putStr "R: " *> B8.putStrLn b *> Telnet.telnetSend t b
+telnetH _ _ (Telnet.Received b)
+  = putStr "R: " *> B8.putStrLn b
 telnetH s _ (Telnet.Send b)
   = putStr "S: " *> send s b
 telnetH _ _ _ = pure ()
@@ -18,4 +19,5 @@ main :: IO ()
 main = connect "127.0.0.1" "telnet" (\(s, _) -> handle s) where
   handle s = do
       telnet <- Telnet.telnetInit [] [] (telnetH s)
-      Telnet.telnetSend telnet "lalala"
+      whileJust_ (recv s 4096) $ \bs -> do
+          Telnet.telnetRecv telnet bs
