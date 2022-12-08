3ba79699295932da4f94abada342167d11a09e39
wldhx <wldhx@wldhx.me>
2022-12-08 19:59:37 +0000

    send telnet

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings#-}
+
 module Main where
 
+import qualified Data.ByteString.Char8 as B8
+import           Network.Simple.TCP (Socket, send, connect)
+import qualified Network.Telnet.LibTelnet as Telnet
+
+
+telnetH :: Socket -> Telnet.EventHandler
+telnetH _ t (Telnet.Received b)
+  = putStr "R: " *> B8.putStrLn b *> Telnet.telnetSend t b
+telnetH s _ (Telnet.Send b)
+  = putStr "S: " *> send s b
+telnetH _ _ _ = pure ()
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = connect "127.0.0.1" "telnet" (\(s, _) -> handle s) where
+  handle s = do
+      telnet <- Telnet.telnetInit [] [] (telnetH s)
+      Telnet.telnetSend telnet "lalala"
--- flake.nix
+++ flake.nix
@@ -5,27 +5,30 @@
   outputs = { self, nixpkgs, flake-utils }:
     {
       overlay = (final: prev: {
-        whohome = final.haskellPackages.callCabal2nix "whohome" ./. { };
+        whohome = final.haskell.packages.ghc94.callCabal2nix "whohome" ./. { };
+      });
+      haskellOverrides = (self0: super0: {
+        haskell = super0.haskell // {
+          packageOverrides = self: super: {
+            libtelnet = self0.haskell.lib.doJailbreak super.libtelnet;
+          };
+        };
       });
     } // flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
+          overlays = [ self.overlay self.haskellOverrides ];
         };
       in {
         packages = { whohome = pkgs.whohome; };
         defaultPackage = self.packages.${system}.whohome;
-        devShell = let haskellPackages = pkgs.haskellPackages;
+        devShell = let haskellPackages = pkgs.haskell.packages.ghc94;
         in haskellPackages.shellFor {
           packages = p: [ self.packages.${system}.whohome ];
           withHoogle = true;
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            ghcid
-            cabal-install
-            nixfmt
-          ];
+          buildInputs = (with pkgs; [ ghcid cabal-install nixfmt ])
+            ++ (with haskellPackages; [ haskell-language-server ]);
           # Change the prompt to show that you are in a devShell
           shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
         };
