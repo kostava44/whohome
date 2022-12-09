951c76690b7603495d263a067acef535451875f8
wldhx <wldhx@wldhx.me>
2022-12-09 19:01:30 +0000

    json config

---
+++ app/Config.hs
@@ -0,0 +1,26 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Config where
+
+import Data.Aeson (FromJSON (..), ToJSON (..))
+import Data.ByteString.Char8 qualified as B8
+import Data.Text.Encoding qualified as T
+import GHC.Generics
+import Network.Simple.TCP (HostName)
+
+instance ToJSON B8.ByteString where
+  toJSON = toJSON . T.decodeUtf8
+
+instance FromJSON B8.ByteString where
+  parseJSON o = T.encodeUtf8 <$> parseJSON o
+
+data Host = Host
+  { hostname :: HostName,
+    username :: B8.ByteString,
+    password :: B8.ByteString
+  }
+  deriving (Generic, Show)
+
+instance ToJSON Host
+
+instance FromJSON Host
--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
+import Config
 import Control.Concurrent (threadDelay)
 import Control.Monad (void)
+import Data.Aeson qualified as Aeson
 import Data.ByteString.Char8 qualified as B8
 import ParseMACs qualified as Parse
-import Secret qualified
 import TelnetSimple qualified as Telnet (State, connect, recvAll, send)
 import Text.Megaparsec hiding (State)
 
 sendCommand :: Telnet.State -> B8.ByteString -> IO B8.ByteString
 sendCommand handle cmd = do
   threadDelay 100_000
-  Telnet.send handle cmd
+  Telnet.send handle (cmd <> "\n")
   threadDelay 100_000
   Telnet.recvAll handle
 
@@ -21,17 +23,24 @@ voidCmd :: Telnet.State -> B8.ByteString -> IO ()
 voidCmd = (fmap . fmap) void sendCommand
 
 main :: IO ()
-main =
+main = do
+  config <-
+    Aeson.eitherDecodeFileStrict' "./config.json" >>= \case
+      Left e -> fail e
+      Right x -> pure x
   void $
     Telnet.connect
-      Secret.destHost
+      (hostname config)
       ( \telnet -> do
-          Secret.auth (voidCmd telnet)
-          buf <- sendCommand telnet "iwinfo wl0 assoclist\n"
+          voidCmd telnet (username config)
+          voidCmd telnet (password config)
+
+          buf <- sendCommand telnet "iwinfo wl0 assoclist"
           let macs =
                 Parse.encode <$> case parse Parse.macsLenient "" buf of
                   Left e -> fail (errorBundlePretty e)
                   Right x -> x
           print macs
-          voidCmd telnet "exit\n"
+
+          voidCmd telnet "exit"
       )
