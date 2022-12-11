6d62068c765a591d5d88b9b0f0a4d1501ef0b344
wldhx <wldhx@wldhx.me>
2022-12-11 17:54:11 +0000

    add static build

--- flake.nix
+++ flake.nix
@@ -8,9 +8,41 @@
 
   outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     {
-      overlay = (final: prev: {
-        whohome = final.haskell.packages.ghc94.callCabal2nix "whohome" ./. { };
-      });
+      overlay = (self: super:
+        {
+          libtelnet = self.callPackage
+            (
+              { lib, stdenv, fetchFromGitHub, pkg-config, cmake, zlib }:
+
+              stdenv.mkDerivation {
+                pname = "libtelnet";
+                version = "0.30.0";
+
+                src = fetchFromGitHub {
+                  owner = "seanmiddleditch";
+                  repo = "libtelnet";
+                  rev = "5f5ecee776b9bdaa4e981e5f807079a9c79d633e";
+                  sha256 = "sha256-4jsS34/aHHb7ojxIlIkEZqsKU11Z6xe9chxXccfa+Kk=";
+                };
+
+                patches = [ ./libtelnet.patch ];
+
+                nativeBuildInputs = [ cmake ];
+                buildInputs = [ zlib ];
+
+                meta = {
+                  description = "Simple RFC-complient TELNET implementation as a C library";
+                  homepage = "https://github.com/seanmiddleditch/libtelnet";
+                  license = lib.licenses.publicDomain;
+                  maintainers = [ lib.maintainers.tomberek ];
+                  platforms = lib.platforms.linux;
+                };
+              }
+            )
+            { };
+          whohome =
+            self.haskell.packages.ghc94.callCabal2nix "whohome" ./. { };
+        });
       haskellOverrides = (self0: super0: {
         haskell = super0.haskell // {
           packageOverrides = self: super: {
@@ -26,7 +58,10 @@
         };
       in
       {
-        packages = { whohome = pkgs.whohome; };
+        packages = {
+          whohome = pkgs.whohome;
+          whohome_static = pkgs.pkgsStatic.whohome;
+        };
         defaultPackage = self.packages.${system}.whohome;
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
