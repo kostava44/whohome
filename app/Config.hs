ee110f485211a6f2d246d25939ec679d26e13455
wldhx <wldhx@wldhx.me>
2023-01-25 20:33:33 +0000

    impl optional mac names

--- app/Config.hs
+++ app/Config.hs
@@ -2,8 +2,9 @@
 
 module Config where
 
-import Data.Aeson (FromJSON (..), ToJSON (..))
+import Data.Aeson (FromJSON (..), FromJSONKey (..), FromJSONKeyFunction (..), ToJSON (..))
 import Data.ByteString.Char8 qualified as B8
+import Data.HashMap.Strict (HashMap)
 import Data.Text (Text)
 import Data.Text.Encoding qualified as T
 import GHC.Generics
@@ -14,6 +15,17 @@ instance ToJSON B8.ByteString where
 
 instance FromJSON B8.ByteString where
   parseJSON o = T.encodeUtf8 <$> parseJSON o
+
+instance FromJSONKey B8.ByteString where
+  fromJSONKey = FromJSONKeyText T.encodeUtf8
+
+data Config = Config
+  { hosts :: [Host],
+    mac_names :: HashMap B8.ByteString Text
+  }
+  deriving (Generic, Show)
+
+instance FromJSON Config
 
 data Host = Host
   { label :: Text,
--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,14 @@ module Main where
 
 import Config
 import Control.Concurrent (threadDelay)
-import Control.Monad (forM, void)
+import Control.Monad (void)
 import Data.Aeson qualified as Aeson
 import Data.ByteString.Char8 qualified as B8
 import Data.ByteString.Lazy qualified as BL
 import Data.HashMap.Strict qualified as HashMap
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as T
 import ParseMACs qualified as Parse
 import TelnetSimple qualified as Telnet (State, connect, recvAll, send)
 import Text.Megaparsec hiding (State, label)
@@ -40,15 +43,18 @@ fetchMacs Host {..} =
             Right x -> x
     )
 
+processHost :: Config -> Host -> IO (Text, [Text])
+processHost Config {mac_names} host = do
+  macs <- fetchMacs host
+  let macsWithNames = [HashMap.findWithDefault (T.decodeUtf8 m) m mac_names | m <- macs]
+  let l = Config.label host
+  pure (l, macsWithNames)
+
 main :: IO ()
 main = do
-  config <-
+  config@Config {..} <-
     (B8.getContents >>=) $
       (. Aeson.eitherDecodeStrict) $ \case
         Left e -> fail e
         Right x -> pure x
-  macs <- forM config $ \c -> do
-    m <- fetchMacs c
-    let l = Config.label c
-    pure (l, m)
-  BL.putStr $ Aeson.encode $ HashMap.fromList macs
+  mapM (processHost config) hosts >>= BL.putStr . Aeson.encode . HashMap.fromList
